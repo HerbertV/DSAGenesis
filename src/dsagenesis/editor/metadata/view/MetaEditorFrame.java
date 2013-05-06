404eff46833e5d9662d932b1be05424de341f9dd
Herbert Veitengruber <tohklidan@hotmail.com>
2013-05-06 18:57:09 +0200

    added abstract layer for genesis related JFrame and JWindow.

---
+++ src/dsagenesis/editor/metadata/view/MetaEditorFrame.java
@@ -0,0 +1,29 @@
+/*
+ *  __  __      
+ * /\ \/\ \  __________   
+ * \ \ \_\ \/_______  /\   
+ *  \ \  _  \  ____/ / /  
+ *   \ \_\ \_\ \ \/ / / 
+ *    \/_/\/_/\ \ \/ /  
+ *             \ \  /
+ *              \_\/
+ *
+ * -----------------------------------------------------------------------------
+ * @author: Herbert Veitengruber 
+ * @version: 1.0.0
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (c) 2013 Herbert Veitengruber 
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+package dsagenesis.editor.metadata.view;
+
+import dsagenesis.core.view.AbstractGenesisFrame;
+
+public class MetaEditorFrame 
+		extends AbstractGenesisFrame 
+{
+
+}
