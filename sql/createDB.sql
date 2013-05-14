100782f6c5bbb9d5d3f51610507c00b7be4ecd48
Herbert Veitengruber <tohklidan@hotmail.com>
2013-05-15 00:59:27 +0200

    Skt and Characteristics.

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -59,12 +59,15 @@ CREATE TABLE "TableColumnLabels" (
 	SKT short for "Steigerungskosten-Tabelle".
 	
 	see: http://www.wiki-aventurica.de/wiki/SKT
+	
+	A*, A** and beyond:
+	for every * cost is reduced by 2 point but minimum is 1.
+	
 */
 DROP TABLE IF EXISTS "SKT";
 
 CREATE TABLE "SKT" (
 	"ID" INTEGER  PRIMARY KEY NOT NULL,
-	"skt_a_star" INTEGER NOT NULL,
 	"skt_a" INTEGER NOT NULL,
 	"skt_b" INTEGER NOT NULL,
 	"skt_c" INTEGER NOT NULL,
@@ -77,25 +80,96 @@ CREATE TABLE "SKT" (
 
 /*
 -----------------------------------------------------------------
+	SKTShifts
+-----------------------------------------------------------------
+	Advantages/Disadvantages special abities can modify the
+	SKT column for a talent/spell etc. 
+	This is called skt shift.
+	
+	If a single source shifts more than one targets
+	you will need multiple entries here.
+	
+		skts_ref_source_ID: the source that triggers the shift
+		skts_ref_target_ID: the target which is shifted
+		
+	shifting options:
+		
+		skts_is_down_shift: 
+		skts_is_up_shift:
+		skts_shift_factor: determinates how many times a shift
+			is applied to the target in most cases 1
+		skts_is_absolute_shift:
+		skts_skt_column: if the shift is absolute you can reference 
+			the SKT column here
+	
+*/
+DROP TABLE IF EXISTS "SKTShifts";
+
+CREATE TABLE "SKTShifts" (
+	"ID" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	"skts_ref_source_ID" VARCHAR(10) NOT NULL,
+	"skts_ref_target_ID" VARCHAR(10) NOT NULL,
+	"skts_is_down_shift" BOOLEAN DEFAULT 'true' NOT NULL,
+	"skts_is_up_shift" BOOLEAN DEFAULT 'false' NOT NULL,
+	"skts_shift_factor" INTEGER DEFAULT '0' NOT NULL,
+	"skts_is_absolute_shift" BOOLEAN DEFAULT 'false' NOT NULL,
+	"skts_skt_column" VARCHAR(10) DEFAULT '' NULL
+);
+
+/*
+-----------------------------------------------------------------
+	CharacteristicGroups
+-----------------------------------------------------------------
+			
+*/
+DROP TABLE IF EXISTS "CharacteristicGroups";
+
+CREATE TABLE "CharacteristicGroups" (
+	"ID" VARCHAR(10) PRIMARY KEY NOT NULL,
+	"cg_name" TEXT NOT NULL
+);
+
+/*
+-----------------------------------------------------------------
 	Characteristics
 -----------------------------------------------------------------
 	contains Attributes and anything else that is leved or calculated
+		
+		c_priorty: is the sort order for displaying the entries.
+		c_acronym: short name
+		c_name: name
+		c_ref_cg_ID: reference to CharacteristicGroups
+		c_cp_cost: creation point cost per characteristic point
+			during the hero creation.
+	
+	for characteristics the can leveled:
+		
+		c_min_value: minimum value 
+		c_max_value: maximum value
+		c_can_increase: is allowed to increase (all attributes)
+		c_can_decrease: is allowed to decrease (e.g. Sozialstatus)
+		c_skt_column: reference to skt column 
+			if null the cost is free or calculated otherwise.
+		c_has_Formular:
+		c_formular:		
 */
 DROP TABLE IF EXISTS "Characteristics";
 
 CREATE TABLE "Characteristics" (
 	"ID" VARCHAR(10) PRIMARY KEY NOT NULL,
+	"c_priorty" INTEGER DEFAULT '0' NOT NULL,
 	"c_acronym" VARCHAR(5) NOT NULL,
 	"c_name" TEXT NOT NULL,
+	"c_ref_cg_ID" VARCHAR(10) REFERENCES CharacteristicGroups(ID) NOT NULL,
+	"c_cp_cost" INTEGER DEFAULT '0' NOT NULL
 	"c_min_value" INTEGER NULL,
 	"c_max_value" INTEGER NULL,
-	"c_is_attribute" BOOLEAN DEFAULT 'false' NOT NULL,
-	"c_can_level" BOOLEAN DEFAULT 'false' NOT NULL,
-	"c_skt_column" VARCHAR(5) DEFAULT '' NULL,
+	"c_can_increase" BOOLEAN DEFAULT 'false' NOT NULL,
+	"c_can_decrease" BOOLEAN DEFAULT 'false' NOT NULL,
+	"c_skt_column" VARCHAR(10) DEFAULT '' NULL,
 	"c_has_Formular" BOOLEAN DEFAULT 'false' NOT NULL,
-	"c_formular" TEXT NULL,
-	
-	-- TODO
+	"c_formular" TEXT NULL
 );
+
 
 
--- sql/createIndexAndLabels.sql
+++ sql/createIndexAndLabels.sql
@@ -1,44 +1,173 @@
-ï»¿/*
-	SKT Entries
+/*
+	SKT
 */
 INSERT INTO CoreDataTableIndex
-		( ti_table_name, ti_prefix, ti_label, ti_note, ti_editable )
-		VALUES
-		( 'SKT', '', 'SKT', 'Steigerungskosten-Tabelle','true');
+	( ti_table_name, ti_prefix, ti_label, ti_note, ti_editable )
+	VALUES
+	( 	'SKT', 
+		'skt_', 
+		'SKT', 
+		'Steigerungskosten-Tabelle',
+		'true'
+	);
 	
 INSERT INTO TableColumnLabels
-		( tcl_table_name, tcl_column_name, tcl_label )
-		VALUES
-		( 'SKT', 'skt_a_start', 'A*');
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKT', 'skt_a', 'A');
 INSERT INTO TableColumnLabels
-		( tcl_table_name, tcl_column_name, tcl_label )
-		VALUES
-		( 'SKT', 'skt_a', 'A');
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKT', 'skt_b', 'B');
 INSERT INTO TableColumnLabels
-		( tcl_table_name, tcl_column_name, tcl_label )
-		VALUES
-		( 'SKT', 'skt_b', 'B');
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKT', 'skt_c', 'C');
 INSERT INTO TableColumnLabels
-		( tcl_table_name, tcl_column_name, tcl_label )
-		VALUES
-		( 'SKT', 'skt_c', 'C');
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKT', 'skt_d', 'D');
 INSERT INTO TableColumnLabels
-		( tcl_table_name, tcl_column_name, tcl_label )
-		VALUES
-		( 'SKT', 'skt_d', 'D');
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKT', 'skt_e', 'E');
 INSERT INTO TableColumnLabels
-		( tcl_table_name, tcl_column_name, tcl_label )
-		VALUES
-		( 'SKT', 'skt_e', 'E');
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKT', 'skt_f', 'F');
 INSERT INTO TableColumnLabels
-		( tcl_table_name, tcl_column_name, tcl_label )
-		VALUES
-		( 'SKT', 'skt_f', 'F');
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKT', 'skt_g', 'G');
 INSERT INTO TableColumnLabels
-		( tcl_table_name, tcl_column_name, tcl_label )
-		VALUES
-		( 'SKT', 'skt_g', 'G');
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKT', 'skt_h', 'H');
+	
+
+/*
+	SKTShifts
+*/
+INSERT INTO CoreDataTableIndex
+	( ti_table_name, ti_prefix, ti_label, ti_note, ti_editable )
+	VALUES
+	(	'SKTShifts', 
+		'skts_', 
+		'SKT Verschiebungen', 
+		'<html>Hier werden die Verschiebungen der SKT Spalten durch Vor-/Nachteile Sonderfähigkeiten verwaltet.<br></html>',
+		'true'
+	);
+	
 INSERT INTO TableColumnLabels
-		( tcl_table_name, tcl_column_name, tcl_label )
-		VALUES
-		( 'SKT', 'skt_h', 'H');
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKTShifts', 'skts_ref_source_ID', 'Quelle');	
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKTShifts', 'skts_ref_target_ID', 'Ziel');	
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKTShifts', 'skts_is_down_shift', 'Ist Links');	
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKTShifts', 'skts_is_up_shift', 'Ist Rechts');	
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKTShifts', 'skts_shift_factor', 'Faktor');	
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKTShifts', 'skts_is_absolute_shift', 'Ist Absolut');	
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'SKTShifts', 'skts_skt_column', 'SKT Spalte');	
+
+
+/*
+	CharacteristicGroups
+*/
+INSERT INTO CoreDataTableIndex
+	( ti_table_name, ti_prefix, ti_label, ti_note, ti_editable )
+	VALUES
+	(	'CharacteristicGroups', 
+		'cg_', 
+		'Grundwerte Gruppen', 
+		'Die Grundwerte sind zur besseren Unterscheidung in Gruppen unterteilt',
+		'false'
+	);
+	
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'CharacteristicGroups', 'cg_name', 'Name');	
+	
+/*
+	Characteristics
+*/		
+
+-- TODO needs update for the new fields
+INSERT INTO CoreDataTableIndex
+	( ti_table_name, ti_prefix, ti_label, ti_note, ti_editable )
+	VALUES
+	( 	'Characteristics', 
+		'c_', 
+		'Grundwerte', 
+		'Eigenschaften und sonstige Werte die berechnet werden oder gesteigert werden dürfen.',
+		'true'
+	);
+
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_priorty', 'Reihenfolge');	
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_acronym', 'kz.');
+		
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_name', 'Name');	
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_ref_cg_ID', 'Gruppe');
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_cp_cost', 'GP');
+
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_min_value', 'Min');
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_max_value', 'Max');		
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_can_increase', 'Steigerbar');
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_can_decrease', 'Senkbar');
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_skt_column', 'SKT Spalte');
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_has_Formular', 'hat Formel');
+INSERT INTO TableColumnLabels
+	( tcl_table_name, tcl_column_name, tcl_label )
+	VALUES
+	( 'Characteristics', 'c_formular', 'Berechnungsformel');
