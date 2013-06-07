86443ff19d85b079f7df374877de1b26c1e4e702
Herbert Veitengruber <tohklidan@hotmail.com>
2013-06-07 19:54:48 +0200

    added Colors for ui coloring.

---
+++ src/dsagenesis/core/ui/Colors.java
@@ -0,0 +1,38 @@
+/*
+ *  ___  ___   _      ___                 _    
+ * |   \/ __| /_\    / __|___ _ _  ___ __(_)___
+ * | |) \__ \/ _ \  | (_ / -_) ' \/ -_|_-< (_-<
+ * |___/|___/_/ \_\  \___\___|_||_\___/__/_/__/
+ *
+ * -----------------------------------------------------------------------------
+ * @author: Herbert Veitengruber 
+ * @version: 1.0.0
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (c) 2013 Herbert Veitengruber 
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+package dsagenesis.core.ui;
+
+import java.awt.Color;
+
+/**
+ * A collection of custom colors used by DSA Genesis UI 
+ */
+public class Colors 
+{
+	public static final Color colorTableRowInactive1 = Color.decode("#ddecff");
+	
+	public static final Color colorTableRowInactive2 = Color.decode("#cae2ff");
+	
+	public static final Color colorTableRowActive = Color.decode("#6aacff");
+	
+	public static final Color colorTableRowActiveReadonly = Color.decode("#b76767");
+	
+	public static final Color colorTableCellActive = Color.decode("#4177b9");
+	
+	public static final Color colorTableCellActiveReadonly = Color.decode("#9e2e2e");
+	
+}
